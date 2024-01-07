e06fd975f63ef5072caf03a76b9b43da2f871f90
fomrus <59192293+fomrus@users.noreply.github.com>
2024-01-07 03:36:01 +0300

    Update archrus1.sh

--- archrus1.sh
+++ archrus1.sh
@@ -75,6 +75,4 @@ pacstrap /mnt base base-devel linux linux-firmware nano dhcpcd netctl
 echo '3.3 Настройка системы'
 genfstab -pU /mnt >> /mnt/etc/fstab
 
-#/path/archrus2.sh
-
-arch-chroot /mnt sh -c "$(sh archuefi2.sh)"
+arch-chroot /mnt sh -c "$(curl -fsSL git.io/archuefi2.sh)"
